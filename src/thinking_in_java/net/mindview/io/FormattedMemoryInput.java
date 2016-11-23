5e79fddc37d3810dc2e4e24a3390b4240c74e464
tianyuan <tianyuan@chinamobile.com>
2016-11-23 16:28:41 +0800

    OCT REM:添加I/O处理的方法

---
+++ src/thinking_in_java/net/mindview/io/FormattedMemoryInput.java
@@ -0,0 +1,19 @@
+package thinking_in_java.net.mindview.io;
+//: io/FormattedMemoryInput.java
+import java.io.*;
+
+public class FormattedMemoryInput {
+  public static void main(String[] args)
+  throws IOException {
+    try {
+      DataInputStream in = new DataInputStream(
+        new ByteArrayInputStream(
+         BufferedInputFile.read(
+          "D:\\Design\\GitWorkspace\\thinking_in_java\\src\\thinking_in_java\\net\\mindview\\io\\FormattedMemoryInput.java").getBytes()));
+      while(in.available() != 0)
+        System.out.print((char)in.readByte());
+    } catch(EOFException e) {
+      System.err.println("End of stream");
+    }
+  }
+} /* (Execute to see output) *///:~
