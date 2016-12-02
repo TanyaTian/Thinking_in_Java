724aaafa5930f28638b9e7f36e84ad3f4dc3ef44
tianyuan <tianyuan@chinamobile.com>
2016-12-02 10:39:20 +0800

    OCT REM:添加stream相关新代码

---
+++ src/thinking_in_java/net/mindview/util/OSExecuteException.java
@@ -0,0 +1,12 @@
+//: net/mindview/util/OSExecuteException.java
+package thinking_in_java.net.mindview.util;
+
+public class OSExecuteException extends RuntimeException {
+  /**
+     * @名称：serialVersionUID
+     * @类型：long
+     */
+    private static final long serialVersionUID = 1L;
+
+public OSExecuteException(String why) { super(why); }
+} ///:~
