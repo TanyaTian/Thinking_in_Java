ec7b5e91cdaf1aa6383d625a9c8a7006a41a289d
tianyuan <tianyuan@chinamobile.com>
2017-04-07 17:16:49 +0800

    OCT REM:添加并发相关代码示例

---
+++ src/thinking_in_java/net/mindview/concurrency/MonitorTest.java
@@ -0,0 +1,55 @@
+package thinking_in_java.net.mindview.concurrency;
+
+
+
+import java.util.concurrent.TimeUnit;
+
+class MyObject implements Runnable {
+    private Monitor monitor;
+
+    public MyObject(Monitor monitor) {
+      this.monitor = monitor;
+    }
+
+    public void run() {
+      try {
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("i'm going.");
+        monitor.gotMessage();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+}
+
+class Monitor implements Runnable {
+    //private volatile boolean go = false;
+
+    public synchronized void gotMessage() throws InterruptedException {
+      //go = true;
+      notify();
+    }
+
+    public synchronized void watching() throws InterruptedException {
+      //if(go == false)
+        wait();
+      System.out.println("He has gone.");
+    }
+
+    public void run() {
+      try {
+        watching();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+}
+
+public class MonitorTest {
+    public static void main(String[] args) {
+      Monitor monitor = new Monitor();
+      MyObject o = new MyObject(monitor);
+      new Thread(o).start();
+      new Thread(monitor).start();
+    }
+}
---
+++ src/thinking_in_java/net/mindview/util/Generator.java
@@ -0,0 +1,4 @@
+//: net/mindview/util/Generator.java
+// A generic interface.
+package thinking_in_java.net.mindview.util;
+public interface Generator<T> { T next(); } ///:~
